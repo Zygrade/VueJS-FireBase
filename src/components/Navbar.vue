c9e84b3b335fd58c42c4a516e118cf5570503f71
Zygrade <hsenday.6991@gmail.com>
2018-10-26 17:54:44 +0530

    P1

--- src/components/Navbar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nav>
-    <div class="nav-wrapper red">
-      <div class="container">
-        <router-link to="/" class="brand-logo">POKEDEX</router-link>
-      </div>
-    </div>
-  </nav>
-</template>
--- src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyBWr7Aba8GYnl7Z7m5gj9HHgQO3yvO9qrk",
-    authDomain: "vuejs-project-d507b.firebaseapp.com",
-    databaseURL: "https://vuejs-project-d507b.firebaseio.com",
-    projectId: "vuejs-project-d507b",
-    storageBucket: "vuejs-project-d507b.appspot.com",
-    messagingSenderId: "929520512349"
-}
